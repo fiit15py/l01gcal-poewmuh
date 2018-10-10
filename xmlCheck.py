814fbd90ca31aaf710c9f932167d53f385fe1a47
poewmuh <grandow78@gmail.com>
2018-10-10 21:20:06 +0900

    delete unnecessary file

--- xmlCheck.py
+++
@@ -1,7 +0,0 @@
-import os
-import xlrd
-book = xlrd.open_workbook('C:/1/l01gcal-poewmuh/imi2018.xls')
-sh = book.sheets()[8]
-for i in range(38):
-        print (sh.cell(i,8).value)
-        print (sh.cell(i,10).value)
