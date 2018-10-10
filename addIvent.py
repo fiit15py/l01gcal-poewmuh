8563104a65d9dffa45c5fbcd9d07e40f2fd62b20
unknown <grandow78@gmail.com>
2018-10-10 16:27:27 +0900

    add ivent in one day

--- addIvent.py
+++ addIvent.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import datetime
+import os
+import xlrd
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -21,32 +23,37 @@ def main():
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    book = xlrd.open_workbook('C:/1/l01gcal-poewmuh/imi2018.xls')
+    sh = book.sheets()[8]
+    start = ['08:00', '9:50', '11:40', '14:00', '15:40', '17:30']
+    end =  ['09:35', '11:25', '13:15', '15:35', '17:20', '19:05']
+    for i in range(38):
+    	if sh.cell(i,8).value != '':
+            event = {
+                'summary': sh.cell(i,8).value,
+                'location': sh.cell(i,10).value,
+                'description': sh.cell(i,9).value,
+                'start': {
+                    'dateTime': '2018-10-08T'+ start[(i-3) % 6] +':00+09:00',
+                    'timeZone': 'Asia/Yakutsk',
+                },
+                'end': {
+                    'dateTime': '2018-10-08T'+ end[(i-3) % 6] +':00+09:00',
+                    'timeZone': 'Asia/Yakutsk',
+                },
+                'recurrence': [
+                    'RRULE:FREQ=WEEKLY;COUNT=12'
+                ],
+                'reminders': {
+                    'useDefault': False,
+                    'overrides': [
+                     {'method': 'popup', 'minutes': 30},
+                     ],
+                }
+                }
 
-    event = {
-      'summary': 'ДВ. Администрирование OC Windows Леверьев В.С.',
-      'location': '430',
-      'description': 'л / пр',
-      'start': {
-        'dateTime': '2018-10-08T15:45:00+09:00',
-        'timeZone': 'Asia/Yakutsk',
-      },
-      'end': {
-        'dateTime': '2018-10-08T17:20:00+09:00',
-        'timeZone': 'Asia/Yakutsk',
-      },
-      'recurrence': [
-        'RRULE:FREQ=WEEKLY;COUNT=12'
-      ],
-      'reminders': {
-      'useDefault': False,
-      'overrides': [
-      {'method': 'popup', 'minutes': 30},
-    ],
-      }
-    }
-
-    event = service.events().insert(calendarId='primary', body=event).execute()
-    print('Event created: %s' % (event.get('htmlLink')))
+            event = service.events().insert(calendarId='primary', body=event).execute()
+            print('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == '__main__':
     main()
